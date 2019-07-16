d4f0240b62d5478b68c0f50bf141e5977c2a940d
Kawsar Hussen <kawsarhussen@gmail.com>
2019-07-16 18:17:37 -0400

    update

--- conways/src/App.js
+++ conways/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Makearray from './component/MakeArray.js';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Graph :</h1>
-        <Makearray />
-      </header>
-    </div>
-  );
+import MakeDimension from './component/MakeDimension.js';
+
+class  App extends Component{
+  render(){
+    return (
+      <div className="App">
+          <h1> Graph :</h1>
+          <MakeDimension/>
+      </div>
+    );
+  }
 }
 
 export default App;
